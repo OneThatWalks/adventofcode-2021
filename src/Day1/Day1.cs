3e4f322e389a09f2c7f681b953d02d3fb9830ddb
OneThatWalks <onethatwalks@aol.com>
2021-12-01 10:18:56 -0500

    Add day 1 and realize we don't want to log into aoc

---
+++ src/Day1/Day1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Day1
+    {
+
+        internal static HttpClient _httpClient = new HttpClient() { };
+        internal const string _baseUrl = "https://adventofcode.com/";
+        internal const string _inputUrl = "2021/day/1/input";
+
+        internal async Task SolveAsync()
+        {
+            var input = await GetInputAsync();
+        }
+
+        private async Task<IEnumerable<int>> GetInputAsync()
+        {
+
+            Console.WriteLine($"Retrieving input from {_baseUrl}");
+
+            var uri = new Uri(new Uri(_baseUrl), _inputUrl);
+
+            var response = await _httpClient.GetAsync(uri);
+
+            Console.WriteLine($"Processing response from {uri}");
+
+            var rawStrResponse = await response.Content.ReadAsStringAsync();
+
+            var splitStrResponse = rawStrResponse.Trim().Split(Environment.NewLine);
+
+            Console.WriteLine($"Received input [{Environment.NewLine}{string.Join(Environment.NewLine, splitStrResponse)}{Environment.NewLine}]");
+
+            return null;
+        }
+    }
+}
--- src/Day1/Program.cs
+++ src/Day1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Day1.Day1 day1 = new Day1.Day1();
+
+await day1.SolveAsync();
+
