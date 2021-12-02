7e167a6253fdb176f9b00cd5fa7d5d64c5e03a01
OneThatWalks <onethatwalks@aol.com>
2021-12-02 16:31:17 -0500

    Solve Day 2 part 1

---
+++ src/Day2/Day2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Day2
+    {
+
+        internal async Task SolveAsync()
+        {
+            var input = await GetInputAsync();
+
+            var resultTuple = CalculateFinalHorizontalAndDepthPosition(input);
+
+            Console.WriteLine($"Final Horizontal Position: {resultTuple.horizontal}");
+            Console.WriteLine($"Final Depth: {resultTuple.depth}");
+            Console.WriteLine($"Final Distance: {resultTuple.depth * resultTuple.horizontal}");
+
+        }
+
+        private (int horizontal, int depth) CalculateFinalHorizontalAndDepthPosition(IEnumerable<(string direction, int delta)> input)
+        {
+            var inputAsArray = input.ToArray();
+            var horizontal = 0;
+            var depth = 0;
+
+            for (int i = 0; i < inputAsArray.Length; i++)
+            {
+                var direction = inputAsArray[i].direction;
+                var delta = inputAsArray[i].delta;
+
+                switch (direction)
+                {
+                    case "forward":
+                        horizontal += delta;
+                        break;
+                    case "down":
+                        depth += delta;
+                        break;
+                    case "up":
+                        depth -= delta;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return (horizontal, depth);
+        }
+
+        private static async Task<IEnumerable<(string direction, int delta)>> GetInputAsync()
+        {
+            Console.WriteLine("Retrieving input from Day2.txt");
+
+            var input = await File.ReadAllTextAsync("Day2.txt");
+
+            var splitStrResponse = input.Trim().Split(Environment.NewLine);
+
+            Console.WriteLine($"Received input [{Environment.NewLine}{string.Join(Environment.NewLine, splitStrResponse)}{Environment.NewLine}]");
+
+            return splitStrResponse.Select(_ =>
+            {
+                var directionDeltaStrings = _.Split(' ');
+
+                return (directionDeltaStrings[0], int.Parse(directionDeltaStrings[1]));
+            });
+        }
+    }
+}
---
+++ src/Day2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Day2.Day2 day2 = new Day2.Day2();
+
+await day2.SolveAsync();
+
